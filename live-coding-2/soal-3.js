19ed769320c7bf6d8ba281d35db492f964d8d32b
ArkanFauzan <arkan@icreativelabs.com>
2023-12-02 12:03:31 +0700

    Ad : jawaban soal 3

--- live-coding-2/soal-3.js
+++ live-coding-2/soal-3.js
@@ -1,11 +1,40 @@
 function tukar_besar_kecil(text){
   // lengkapi kode berikut
   
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    
+    result += text[i] === text[i].toLowerCase() ? text[i].toUpperCase() : text[i].toLowerCase();
+
+    if (text[i] === text[i].toLowerCase()) {
+      result += text[i].toUpperCase();
+    }
+    else {
+      result += text[i].toLowerCase();
+    }
+
+  }
+  return result;
 }
 
 
-console.log(tukar_besar_kecil('Hello World')); // "hELLO wORLD"
-console.log(tukar_besar_kecil('I aM aLAY')); // "i Am Alay"
-console.log(tukar_besar_kecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
-console.log(tukar_besar_kecil('IT sHOULD bE me')); // "it Should Be ME"
-console.log(tukar_besar_kecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+// console.log(tukar_besar_kecil('Hello World')); // "hELLO wORLD"
+// console.log(tukar_besar_kecil('I aM aLAY')); // "i Am Alay"
+// console.log(tukar_besar_kecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+// console.log(tukar_besar_kecil('IT sHOULD bE me')); // "it Should Be ME"
+// console.log(tukar_besar_kecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+
+// konsepnya:
+/*
+let char = "h";
+if (char === char.toLowerCase()) {
+  char = char.toUpperCase();
+  // console.log('ini jadiin kapital');
+}
+else {
+  char = char.toLowerCase();
+  // console.log('ini jadiin kecil');
+}
+
+console.log(char);
+*/
---
+++ live-coding-2/ternary.js
@@ -0,0 +1,33 @@
+
+let huruf = "";
+let karakter = "iola41js4h14dfoil412as41h41lj4k1df4h1au4j1s4h1";
+
+// huruf = huruf + "a";
+// huruf += "c";
+// console.log(huruf);
+
+// huruf += true ? "a" : "";
+// huruf += true ? "c" : "";
+// huruf += false ? "z" : "";
+// huruf += false ? "x" : "";
+// huruf += false ? "j" : "";
+// huruf += true ? "m" : "";
+
+for (let i = 0; i < karakter.length; i++) {
+  huruf += parseInt(karakter[i]) >= 0 ? "" : karakter[i];
+}
+
+console.log(huruf);
+
+
+if ("") {
+  console.log("true");
+}
+else {
+  console.log("false");
+}
+
+
+let bilangan = -1;
+let jenis = bilangan > 0 ? "positif" : ( bilangan === 0 ? "enol" : "negatif" );
+console.log(jenis);
